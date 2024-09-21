12b1d4ef9a428a1451996e1a4bf0f7b4b27c725e
ashen0240 <70177963+ashen0240@users.noreply.github.com>
2024-09-21 16:48:36 -0700

    Add files via upload

---
+++ Amy/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    //run notes
+    System.out.println("\n\nNotes.java\n\n");
+    Notes.main(args);
+    System.out.println("\n\nProblems.java\n\n");
+    Problems.main(args);
+  }
+}

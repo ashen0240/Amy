4cd65ba38ce941d2954102cb8f0bed0d5824a80e
Andy Shen <alshen@usc.edu>
2024-09-21 16:56:58 -0700

    git ignore

--- Amy/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    //run notes
-    System.out.println("\n\nNotes.java\n\n");
-    Notes.main(args);
-    System.out.println("\n\nProblems.java\n\n");
-    Problems.main(args);
-  }
-}
